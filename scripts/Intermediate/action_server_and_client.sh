c0d313302aa4afd0fe323b7de07dd313c51ad8fd
Ri Yevgeniy <risimon@mail.ru>
2023-11-21 00:09:54 +0900

    SLAM Practice: Ocupancy Greed Map is drawn based on robot's odometry, tf and scan information with the help

--- scripts/Intermediate/action_server_and_client.sh
+++ scripts/Intermediate/action_server_and_client.sh
@@ -45,8 +45,6 @@ nano ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
 
 #Let’s try running our action server:
 python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
-nano ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
-python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
-nano ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
-python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_server.py
-gnome-terminal -- bash -c "ros2 action send_goal --feedback fibonacci action_tutorials_interfaces/action/Fibonacci "{order: 5}"; python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; nano ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; exec bash"
+
+
+#gnome-terminal -- bash -c "ros2 action send_goal --feedback fibonacci action_tutorials_interfaces/action/Fibonacci "{order: 5}"; python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; nano ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; python3 ~/Smart-Mobility-Engineering-Lab/fibonacci_action_client.py; exec bash"
